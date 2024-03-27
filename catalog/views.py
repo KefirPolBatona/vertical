f8ce300d67e828736419e69a369d6ddae4fcb18b
evgeni <9232485@gmail.com>
2024-03-27 20:02:32 +0300

    Home_work_19_2 "Getting to know Django". Ready

---
+++ catalog/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'catalog/home.html')
+
+
+def contact(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        print(f'You have new message from {name} ({phone}): {message}')
+    return render(request, 'catalog/contact.html')
+
+
+def instructor(request):
+    return render(request, 'catalog/instructor.html')
